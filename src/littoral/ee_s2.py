0ee1354a09a1efff1615f9c5977c7553cdb89d48
wzesk <walter@conformlab.com>
2025-03-10 23:22:30 +0000

    updated tar to set project path

--- src/littoral/ee_s2.py
+++ src/littoral/ee_s2.py
@@ -365,7 +365,7 @@ def process_collection_images_tofiles(data, se2_col, max_images=-1):
     return proj_track
 
 
-def process_collection_images_totar(data: Dict, se2_col: ee.ImageCollection) -> pd.DataFrame:
+def process_collection_images_totar(data: Dict, se2_col: ee.ImageCollection,site_path='') -> pd.DataFrame:
     """Batch process multiple images from collection.
 
     Parameters
@@ -465,7 +465,10 @@ def process_collection_images_totar(data: Dict, se2_col: ee.ImageCollection) -> 
     proj_track.to_csv(path + "/" + "proj_track.csv", index=False)
 
     date_str = datetime.datetime.now().strftime("%Y-%m-%d")
-    site_path = path.replace(name,"littoral_sites.csv")
+    
+    if len(site_path) < 3:
+        site_path = path.replace(name,"littoral_sites.csv")
+        
     littoral_sites.set_last_run(name, date_str,site_path)
     print(f"finished run: {name} {date_str}")
 
